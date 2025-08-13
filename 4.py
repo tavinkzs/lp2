705bbe530299cfd8e78628dde1bf8545770046e8
otavio augusto <tavigamer08@gmail.com>
2025-08-13 20:24:03 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+altura = float(input("Digite a altura do trapezio: "))
+bm = float(input("Digite a base maior do trapezio: "))
+bme = float(input("Digite a base menor do trapezio: "))
+
+A = ((bm + bme) * altura ) / 2
+
+print("a area do trapezio é: ", A)
