8dae15a92592b9880d579368215583a4b3c7dbb4
otavio augusto <tavigamer08@gmail.com>
2025-08-13 20:24:20 -0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,15 @@
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+
+print("Antes da troca:")
+print("n1 =", n1)
+print("n2 =", n2)
+
+
+n1, n2 = n2, n1
+
+
+print("Depois da troca:")
+print("n1 =", n1)
+print("n2 =", n2)
